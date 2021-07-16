482f5eab943eee52dc9112297389443b88eceacb
rikicop <ruperto1@protonmail.com>
2021-07-16 16:45:02 -0400

    light states

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class CountingParent extends React.Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  state = {
+    cuarto: false,
+    bano: false,
+    sala: false,
+    cocina: false
+  };
+
+  handleCuarto = (action) => {
+    console.log('Child says', action);
+    this.setState({
+      cuarto: !this.state.cuarto
+    });
+    console.log('Estdo Actual: ', this.state.cuarto)
+  }
+
+  handleBano = (action) =>{
+    this.setState({ bano: !this.state.bano });
+  }
+
+  handleSala = (action) =>{
+    this.setState({sala: !this.state.sala});
+  }
+  handleCocina = (action) => {
+    this.setState({cocina: !this.state.cocina});
+  }
+
+  render() {
+    return (
+      <div>
+        <Child
+          label={'Cuarto'}
+          onAction={this.handleCuarto}
+          children={this.state.cuarto ? 'Prendido' : 'Apagado'}
+        />
+        <br />
+        <Child
+          label={'Baño'}
+          onAction={this.handleBano}
+          children={this.state.bano ? 'Prendido' : 'Apagado'}
+        />
+        <br />
+        <Child
+          label={'Sala'}
+          onAction={this.handleSala}
+          children={this.state.sala ? 'Prendido' : 'Apagado'}
+        /><br />
+        <Child
+          label={'Cocina'}
+          onAction={this.handleCocina}
+          children={this.state.cocina ? 'Prendido' : 'Apagado'}
+        />
+      </div>
+
+    );
+  }
+}
+
+function Child({ onAction, children, label }) {
+  return (
+    <>
+      <button onClick={onAction}>
+        {label}
+      </button><br />
+      {children}
+
+    </>
+  );
+}
+
+const Page = () => (
+  <div>
+    <CountingParent />
+
+  </div>
+)
+
+ReactDOM.render(<Page />, document.querySelector('#root'));
